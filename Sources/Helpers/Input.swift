7905c0bc7b34cff6c258da4b946ae7e449abe4de
Antoine Montes <antoine.montes@pickyourskills.com>
2024-12-01 15:30:44 +0100

    format

--- Sources/Helpers/Input.swift
+++ Sources/Helpers/Input.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 enum Part: Int {
-    case one = 1
-    case two = 2
+  case one = 1
+  case two = 2
 }
 
 func getInput(day: Int, part: Part) -> [String] {
-    let dayString: String = String(format: "%02d", day)
-    let path: String = "Sources/Days/Day\(dayString)/input\(part.rawValue).txt"
+  let dayString = String(format: "%02d", day)
+  let path = "Sources/Days/Day\(dayString)/input\(part.rawValue).txt"
 
-    do {
-        let contents: String = try String(contentsOfFile: path)
-        return contents.components(separatedBy: "\n")
-    } catch {
-        fatalError("Could not read file at \(path)")
-    }
+  do {
+    let contents = try String(contentsOfFile: path)
+    return contents.components(separatedBy: "\n")
+  } catch {
+    fatalError("Could not read file at \(path)")
+  }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,2 @@
-let day01Ret: String = day01Part2()
+let day01Ret = day01Part2()
 print(day01Ret)
